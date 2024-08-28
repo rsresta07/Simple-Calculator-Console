9129e1ca9651dbb95d41f61447b70a348ba2bbdf
Rameshwor Shrestha <r.sresta07@gmail.com>
2024-08-28 09:25:58 +0545

    Add project files.

---
+++ Simple Calculator/Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_Calculator
+{
+	internal class Calculator
+	{
+		static void Main(string[] args)
+		{
+			bool keepCalculating = true;
+		}
+	}
+}
